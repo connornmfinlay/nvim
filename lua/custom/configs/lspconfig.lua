98f48436087c10d3073bcb96e3f571942a4abbcc
connornmfinlay <cnm.finlay@gmail.com>
2024-04-26 02:53:12 +0100

    switching go lsp version

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,10 +4,10 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = {"bashls", "pyright", "rust_analyzer", "ansiblels", "dockerls", "bashls", "golangci_lint_ls", "zls"},
+  ensure_installed = {"bashls", "pyright", "rust_analyzer", "ansiblels", "dockerls", "bashls", "gopls", "zls"},
 })
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd","bashls","dockerls","golangci_lint_ls", "zls" }
+local servers = { "html", "cssls", "tsserver", "clangd","bashls","dockerls","gopls", "zls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
